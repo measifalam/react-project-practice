4b23261e61274e611126eb5ffdb8b7eb215abeb9
Md Asif Alam Alam <v-mdaalam@microsoft.com>
2022-04-24 00:21:49 +0530

    master

---
+++ lorem-ipsum/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
+import data from './data'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(count)
+    if (count <= 0) {
+      amount = 1
+    }
+
+    if (count > 8) {
+      amount = 8
+    }
+    setText(data.slice(0, amount))
+  }
+
+  return (
+    <section className='section-center'>
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form action='' className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>Paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn'>Generate</button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
+  )
+}
+
+export default App
